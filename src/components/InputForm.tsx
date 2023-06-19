7dfe54c230210d22fcabdd72d6735567dc42f9e8
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-06-18 23:54:56 -0500

    started speech recognition feature

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { IoMdSend } from "react-icons/io";
+import { BsFillMicFill } from "react-icons/bs";
+
 type InputFormProps = {
   sendMessage: (e: any) => void;
   isResponding: boolean;
@@ -5,12 +9,79 @@ type InputFormProps = {
   setPrompt: (str: string) => void;
 };
 
+interface ISpeechRecognitionWindow extends Window {
+  // @ts-ignore
+  SpeechRecognition: typeof SpeechRecognition;
+  webkitSpeechRecognition: typeof SpeechRecognition;
+  mozSpeechRecognition: typeof SpeechRecognition;
+  msSpeechRecognition: typeof SpeechRecognition;
+}
+
+const {
+  // @ts-ignore
+  SpeechRecognition = window.SpeechRecognition ||
+    // @ts-ignore
+    window.webkitSpeechRecognition ||
+    // @ts-ignore
+    window.mozSpeechRecognition ||
+    // @ts-ignore
+    window.msSpeechRecognition,
+} = window as unknown as ISpeechRecognitionWindow;
+
 const InputForm = ({
   sendMessage,
   isResponding,
   prompt,
   setPrompt,
 }: InputFormProps) => {
+  const [transcript, setTranscript] = useState("");
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    console.log(transcript);
+    setPrompt(transcript);
+  }, [transcript]);
+
+  useEffect(() => {
+    let finalTranscript = "";
+    let speechRecognition = new SpeechRecognition();
+
+    speechRecognition.onstart = () => {
+      console.log("Speech recognition service has started");
+    };
+
+    speechRecognition.onresult = (event: any) => {
+      let interimTranscript = "";
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript + " ";
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+      setTranscript(finalTranscript + interimTranscript);
+    };
+
+    speechRecognition.onerror = (event: any) => {
+      console.log("Error occurred in recognition: " + event.error);
+    };
+
+    if (isListening) {
+      finalTranscript = "";
+      speechRecognition.start();
+      console.log("Listening...");
+    } else {
+      speechRecognition.stop();
+      console.log("Stopped Listening");
+    }
+
+    return () => {
+      speechRecognition.stop();
+    };
+  }, [isListening]);
+
   return (
     <form
       onSubmit={sendMessage}
@@ -22,18 +93,33 @@ const InputForm = ({
         type="text"
         value={prompt}
       />
-      <button
-        disabled={isResponding}
-        type="submit"
-        className={[
-          "text-white font-bold py-2 px-4 rounded sm:col-span-1 md:col-span-1 h-12",
-          isResponding
-            ? "bg-gray-300"
-            : "bg-blue-500 hover:bg-blue-700 cursor-not-allowed",
-        ].join(" ")}
-      >
-        Send Message
-      </button>
+      <div className="sm:col-span-1 md:col-span-1 flex gap-4">
+        <button
+          disabled={isResponding}
+          type="submit"
+          className={[
+            "text-white font-bold py-2 px-4 rounded h-12 w-full",
+            isResponding
+              ? "bg-gray-300"
+              : "bg-blue-500 hover:bg-blue-700 cursor-not-allowed",
+          ].join(" ")}
+        >
+          <IoMdSend className="inline ml-2" />
+        </button>
+        <button
+          disabled={isResponding}
+          onClick={() => setIsListening((prevState) => !prevState)}
+          type="button"
+          className={[
+            "text-white font-bold py-2 px-4 rounded h-12 w-full",
+            isResponding
+              ? "bg-gray-300"
+              : "bg-blue-500 hover:bg-blue-700 cursor-not-allowed",
+          ].join(" ")}
+        >
+          <BsFillMicFill className="inline ml-2" />
+        </button>
+      </div>
     </form>
   );
 };
