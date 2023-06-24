baf504da131daf2382434a9c65c434dc7f4367e5
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-06-24 16:29:36 -0500

    updated styling

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -11,19 +11,19 @@ const Settings = () => {
         <div
           id="backdrop"
           onClick={() => setShowSettings(false)}
-          className="fixed top-0 left-0 h-full w-full bg-black/20"
+          className="fixed top-0 left-0 h-full w-full bg-black/50 transition-opacity duration-500 ease-in-out"
         />
       )}
 
       <div
         id="settings"
-        className={`fixed bottom-0 right-0 left-0 transition-all duration-300 ease-in-out ${
+        className={`fixed bottom-0 right-0 left-0 transition-all duration-500 ease-in-out bg-white shadow-lg border-t-2 border-gray-200 ${
           showSettings ? "h-full pt-8 " : "h-0"
-        } overflow-hidden bg-white shadow-lg `}
+        } overflow-hidden`}
       >
         <div className="p-4 flex">
           <button
-            className="text-2xl hover:opacity-50 flex"
+            className="text-2xl hover:text-gray-500 transition-colors flex"
             onClick={() => setShowSettings(false)}
           >
             <GoChevronDown className="my-auto" />
@@ -36,24 +36,23 @@ const Settings = () => {
             id="settings"
             className="h-full overflow-y-auto shadow-inner bg-gray-50/10 p-2 mb-4 pb-14"
           >
-            {/* add more settings like this, the overflow will handle the scrolling */}
             <div id="prompt" className="flex flex-col h-1/2 my-2">
-              <strong className="mb-2">Prompt</strong>
-              <textarea className="h-full border-2 rounded p-4 resize-none"></textarea>
+              <strong className="mb-2 text-lg">Prompt</strong>
+              <textarea className="h-full border-2 rounded p-4 resize-none focus:outline-none focus:border-blue-400 transition-colors"></textarea>
             </div>
           </div>
 
           <div
             id="actions"
-            className="flex absolute bottom-0 w-full h-20 bg-white shadow-inner justify-end"
+            className="flex absolute bottom-0 w-full h-20 bg-white shadow-inner justify-end items-center"
           >
             <button
               onClick={() => setShowSettings(false)}
-              className="border-2 rounded h-12 w-24 my-auto mx-2"
+              className="border-2 border-gray-300 rounded h-12 w-24 mx-2 hover:border-blue-500 transition-colors"
             >
               Cancel
             </button>
-            <button className="border-2 rounded h-12 w-24 my-auto mx-2">
+            <button className="rounded h-12 w-24 mx-2 bg-blue-500 text-white hover:bg-blue-600 transition-colors">
               Save
             </button>
           </div>
